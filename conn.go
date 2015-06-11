abdfeb162d9b26989203c2d59a2448806450473e
flexconstructor <flexconstructor@gmail.com>
2015-06-11 10:36:24 +0300

    V-2: Adds checkOrigin func to websocket

--- conn.go
+++ conn.go
@@ -29,6 +29,7 @@ const (
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
+	CheckOrigin: func(r *http.Request) bool { return true },
 }
 
 // connection is an middleman between the websocket connection and the hub.
