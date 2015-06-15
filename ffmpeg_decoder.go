972d42f065edd004f9036343a3f488940d6a2e78
flexconstructor <flexconstructor@gmail.com>
2015-06-15 18:15:12 +0300

    V-2: write metadata commented

--- ffmpeg_decoder.go
+++ ffmpeg_decoder.go
@@ -129,14 +129,14 @@ func (f *FFmpegDecoder)Run(){
 		return
 	}
 	f.log.Info("Open stream")
-	if(f.metadata != nil && f.rtmp_status != nil){
+	/*if(f.metadata != nil && f.rtmp_status != nil){
 		f.metadata <- &MetaData{
 			Message: "metadata",
 			Width: srcVideoStream.CodecCtx().Width(),
 			Height: srcVideoStream.CodecCtx().Height(),
 		}
 		f.log.Info("write metadata")
-	}
+	}*/
 
 	wg := new(sync.WaitGroup)
 
