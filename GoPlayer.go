6c371d278cb5a7b3ee46737e7e4325ef62b09758
flexconstructor <flexconstructor@gmail.com>
2015-06-17 16:36:25 +0300

    trace ws connection

--- GoPlayer.go
+++ GoPlayer.go
@@ -121,7 +121,7 @@ func(p *GoPlayer) Run(stream_name string) bool{
 	if(p.streams_map[stream_name] == nil) {
 		p.log.Debug("create new hub")
 		//newhub:= NewHub("rtmp://"+p.rtmp_host+":"+strconv.Itoa(p.rtmp_port)+"/"+p.app_name, stream_name,p.log,p.service_token,p.handler)
-		p.route.HandleFunc("/"+p.app_name+"/"+stream_name, serveWebSocket)
+		p.route.HandleFunc("/"+p.app_name+"/"+stream_name, p.serveWebSocket)
 	//	p.streams_map[stream_name]=newhub
 		//go  newhub.run()
 	}
@@ -141,7 +141,7 @@ func (p *GoPlayer) Close(stream_name string)bool{
 return false
 }
 
-func serveWebSocket(w http.ResponseWriter, r *http.Request){
-	log.Debug("SERVE WEB SOCKET")
+func (p *GoPlayer)serveWebSocket(w http.ResponseWriter, r *http.Request){
+	p.log.Debug("SERVE WEB SOCKET")
 
 }
