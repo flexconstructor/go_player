2f21001977ea451a15fdf497859461bf8cba43a4
flexconstructor <flexconstructor@gmail.com>
2015-06-24 12:27:25 +0300

    V-2: write frame data off correct

--- ffmpeg_encoder.go
+++ ffmpeg_encoder.go
@@ -72,8 +72,9 @@ func (e *FFmpegEncoder)Run(){
 		swsCtx.Scale(srcFrame, dstFrame)
 
 		if p, ready, _ := dstFrame.EncodeNewPacket(cc); ready {
-			e.log.Debug("frame ready")
+			e.log.Debug("frame ready ",p.Size())
 			//e.broadcast <-p.Data()
+
 		}
 		gmf.Release(srcFrame)
 		e.log.Debug("release frame")
