9e946876ee6b569a31fcb2de0b5092c76354d75d
flexconstructor <flexconstructor@gmail.com>
2015-07-02 15:04:21 +0300

    error close rtmp connection

--- hub.go
+++ hub.go
@@ -83,7 +83,7 @@ func (h *hub) run() {
 
 	}
 
-	//defer ff.Close();
+	defer ff.Close();
 
 	h.log.Debug("decoder created")
 
@@ -100,7 +100,7 @@ func (h *hub) run() {
 			 log: h.log,
  		 },
 	}
-	defer conn.Close()
+	//defer conn.Close()
 	h.log.Debug("connection created")
 	defer h.Close()
 	for {
