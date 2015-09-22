7405d3892567c100b2be21cdc1bffa6e6ba129d3
flexconstructor <flexconstructor@gmail.com>
2015-09-22 09:09:21 +0300

    modified get new packet for decoder

--- ffmpeg_decoder.go
+++ ffmpeg_decoder.go
@@ -47,9 +47,8 @@ func (d *FFmpegDecoder) Run() {
 		d.error <- NewErrorWithDescription(1, 1, "Invalid codec")
 		return
 	}
-
-	packets_chan:= inputCtx.GetNewPackets();
 	for{
+		packets_chan:= inputCtx.GetNewPackets();
 		select{
 		case <-d.close_chan:
 			return
