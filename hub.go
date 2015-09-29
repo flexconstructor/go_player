dc1664706374ec4108e1e91b142ccfaeb0415cef
flexconstructor <flexconstructor@gmail.com>
2015-09-29 19:16:37 +0300

    V-4: parse uint correct.

--- hub.go
+++ hub.go
@@ -140,7 +140,7 @@ func (h *hub) run() {
 			return
 		}
 	}*/
-	sock:=fmt.Sprint("/home/mediaapi/nginx/html/temp/dash/%s.sock",strconv.ParseUint(h.model_id,10,64))
+	sock:=fmt.Sprint("/home/mediaapi/nginx/html/temp/dash/%s.sock",strconv.FormatUint(h.model_id,10))
 	fmt.Println("sock %s",sock)
 	return
 	l, err := net.Listen("unix", sock)
